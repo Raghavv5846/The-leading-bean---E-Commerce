c463302cba6dcd948d70d7a59a429a7a0eb6ca36
Raghavv5846 <89181921+Raghavv5846@users.noreply.github.com>
2023-05-24 20:20:57 +0530

    Update view-helpers.js

--- functions/config/view-helpers.js
+++ functions/config/view-helpers.js
@@ -6,6 +6,6 @@ module.exports = (app) => {
     app.locals.assetPath = function(filePath){
 
         // console.log("ahskahkaksj",'/' + JSON.parse(fs.readFileSync(path.join(__dirname, '../public/rev-manifest.json')))[filePath]);
-        return '/' + JSON.parse(fs.readFileSync(path.join(__dirname, '../../public/rev-manifest.json')))[filePath];
+        return '/' + JSON.parse(fs.readFileSync(path.join(__dirname, '../public/rev-manifest.json')))[filePath];
     }
 }
