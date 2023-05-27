d69f1af77f40b460aff8d3575cb4a4caa4f541c4
raghavv5846 <raghav.pareek5846@gmail.com>
2023-05-27 21:36:40 +0530

    commit

--- functions/index.js
+++ functions/index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname,'../public')));
 app.use(expressLayouts);
 app.use(cookieParser());
 app.use(session({
-    name:"__session",
+    name:"tlb",
     secret: 'secret',
     saveUninitialized: false,
     resave: false,
@@ -57,11 +57,11 @@ app.set('view engine','ejs');
 app.set('views','./views');
 app.use(toastrMiddleware.checkUrl);
 app.use('/',require('./routes/index'));
-// app.listen(8000,function(err){
-//     if(err){
-//         console.log(`error while running on 8000`);
-//     }
-//     console.log(`800 Server has been succesfully started`);
-// })
+app.listen(8000,function(err){
+    if(err){
+        console.log(`error while running on 8000`);
+    }
+    console.log(`800 Server has been succesfully started`);
+})
 
 exports.app = functions.https.onRequest(app);
